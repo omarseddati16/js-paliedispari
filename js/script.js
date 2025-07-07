ca639665430a08d603e4c6db057ad94d39f24882
omarseddati16 <omarseddati16@gmail.com>
2025-07-07 14:41:11 +0200

    prova due

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 // Palidroma
-let ask = prompt('immetti una parola ')
 function palidroma(text) {
-  let word = [];
-  const words = text.split(' ');
-  for (let i = 0; i = words; i++)
-    if (words = words) {
-      console.log('la parola è palidroma')
-    }
-    else {
-      console.log('la parola non è palidroma')
-    }
+  let ask = prompt('immetti una parola ')
+  const reversedwords = ask.split('').reverse().join('');
+  if (ask === reversedwords) {
+    console.log('la parola è palidroma')
+  }
+  else {
+    console.log('la parola non è palidroma')
+  }
 }
